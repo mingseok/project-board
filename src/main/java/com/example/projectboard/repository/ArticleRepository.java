121a28651a071cec38cd5a1f6ec30dcb0af09e5f
alstjr706 <alstjr706@naver.com>
2023-08-29 03:50:27 +0900

    feat : (board) 엔티티 연관관계 구현

---
+++ src/main/java/com/example/projectboard/repository/ArticleRepository.java
@@ -0,0 +1,9 @@
+package com.example.projectboard.repository;
+
+import com.example.projectboard.domain.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+
+
+}
---
+++ src/test/java/com/example/projectboard/repository/JpaRepositoryTest.java
@@ -0,0 +1,34 @@
+package com.example.projectboard.repository;
+
+import com.example.projectboard.config.JpaConfig;
+import com.example.projectboard.domain.Article;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DisplayName("JPA 연결 테스트")
+@Import(JpaConfig.class)
+@DataJpaTest
+class JpaRepositoryTest {
+
+    private final ArticleRepository articleRepository;
+    private final ArticleCommentRepository articleCommentRepository;
+
+    public JpaRepositoryTest(@Autowired ArticleRepository articleRepository,
+                             @Autowired ArticleCommentRepository articleCommentRepository) {
+        this.articleRepository = articleRepository;
+        this.articleCommentRepository = articleCommentRepository;
+    }
+
+    @Test
+    void name() {
+        List<Article> articles = articleRepository.findAll();
+        assertThat(articles).isNotNull().hasSize(0);
+    }
+}
