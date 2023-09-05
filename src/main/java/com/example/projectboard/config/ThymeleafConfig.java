1fa2ee39d9f0bfe909544450985da31397c05285
alstjr706 <alstjr706@naver.com>
2023-09-05 20:53:26 +0900

    feat : (board) spring boot configuration processor 디펜던시 추가

---
+++ src/main/java/com/example/projectboard/config/ThymeleafConfig.java
@@ -0,0 +1,28 @@
+package com.example.projectboard.config;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+
+@Configuration
+public class ThymeleafConfig {
+
+    public SpringResourceTemplateResolver thymeleafTemplateResolver(
+            SpringResourceTemplateResolver defaultTemplateResolver,
+            Thymeleaf3Properties thymeleaf3Properties) {
+
+        defaultTemplateResolver.setUseDecoupledLogic(thymeleaf3Properties.isDecoupledLogic());
+        return defaultTemplateResolver;
+    }
+
+    @RequiredArgsConstructor
+    @Getter
+    @ConstructorBinding
+    @ConfigurationProperties("spring.thymeleaf3")
+    public static class Thymeleaf3Properties {
+        private final boolean decoupledLogic;
+    }
+}
