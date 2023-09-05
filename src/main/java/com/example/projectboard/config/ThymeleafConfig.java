f5149b03d46923a537474d958735f8f9253bc1a3
alstjr706 <alstjr706@naver.com>
2023-09-05 21:16:09 +0900

    feat : (board) 시큐리티 설정을 모든 요청 인증에 열리도록 수정

--- src/main/java/com/example/projectboard/config/ThymeleafConfig.java
+++ src/main/java/com/example/projectboard/config/ThymeleafConfig.java
@@ -4,17 +4,20 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 
 @Configuration
 public class ThymeleafConfig {
 
+    @Bean
     public SpringResourceTemplateResolver thymeleafTemplateResolver(
             SpringResourceTemplateResolver defaultTemplateResolver,
             Thymeleaf3Properties thymeleaf3Properties) {
 
         defaultTemplateResolver.setUseDecoupledLogic(thymeleaf3Properties.isDecoupledLogic());
+
         return defaultTemplateResolver;
     }
 
@@ -23,6 +26,9 @@ public class ThymeleafConfig {
     @ConstructorBinding
     @ConfigurationProperties("spring.thymeleaf3")
     public static class Thymeleaf3Properties {
+        /**
+         * Use Thymeleaf 3 Decoupled Logic
+         */
         private final boolean decoupledLogic;
     }
 }
